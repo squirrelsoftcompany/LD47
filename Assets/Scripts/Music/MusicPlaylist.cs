e2dd3b9bedfd33182d3503ecf53d9e8abc09b9a3
Xavier Chalut <xavier.chalut@gmail.com>
2020-10-04 01:50:10 +0200

    [FCT] Update Dora and add sound manager

---
+++ Assets/Scripts/Music/MusicPlaylist.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlaylist : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> mMusicList;
+    [SerializeField] private int mCurrentIndex = 0;
+    [SerializeField] private float mPauseDuration = 0;
+
+    private float mPauseTimer = 0;
+    private float mFadeTime = 5.0f;
+    private bool mFadeStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPauseTimer = mPauseDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!SoundManager.Instance.Music.isPlaying && mCurrentIndex < mMusicList.Count)
+        {
+            mPauseTimer = mPauseTimer + Time.deltaTime;
+            if(mPauseTimer> mPauseDuration)
+            {
+                SoundManager.Instance.PlaySingle(mMusicList[mCurrentIndex]);
+                mCurrentIndex++;
+                mFadeStart = false;
+                mPauseTimer = 0;
+            }
+        }
+        else if (!SoundManager.Instance.Music.isPlaying && mCurrentIndex >= mMusicList.Count)
+        {
+            mCurrentIndex = 0;
+        }
+        else
+        {
+            if(!mFadeStart && SoundManager.Instance.Music.time > SoundManager.Instance.Music.clip.length - mFadeTime)
+            {
+                StartCoroutine(SoundManager.Instance.FadeOut(mFadeTime));
+                mFadeStart = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Music/SoundManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance
+    {
+        get
+        {
+            return m_instance;
+        }
+    }
+    public AudioSource Music
+    {
+        get
+        {
+            return mMusic;
+        }
+        set
+        {
+            mMusic = value;
+        }
+    }
+
+    public static SoundManager m_instance = null;
+    [SerializeField] private AudioSource mMusic;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_instance != null)
+            Destroy(gameObject);
+        else
+            m_instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayLoopSingle(AudioClip clip)
+    {
+        mMusic.clip = clip;
+        mMusic.loop = true;
+        mMusic.Play();
+    }
+
+    public void PlaySingle(AudioClip clip)
+    {
+        mMusic.clip = clip;
+        mMusic.loop = false;
+        mMusic.Play();
+    }
+
+    public IEnumerator FadeOut(float pFadeTime)
+    {
+        float startVolume = mMusic.volume;
+
+        while (mMusic.volume > 0)
+        {
+            mMusic.volume -= startVolume * Time.deltaTime / pFadeTime;
+            yield return null;
+        }
+        mMusic.Stop();
+        mMusic.volume = startVolume;
+    }
+
+    public IEnumerator FadeIn(float pFadeTime)
+    {
+        mMusic.volume = 0;
+        mMusic.Play();
+        while (mMusic.volume < 1)
+        {
+            mMusic.volume += Time.deltaTime / pFadeTime;
+            yield return null;
+        }
+    }
+
+}

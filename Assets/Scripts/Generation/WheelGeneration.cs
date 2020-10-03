c9699ca3528876a24aa616d701efc9f76633ea0d
ChristopheClaustre <christophe.claustre.31@gmail.com>
2020-10-03 14:00:18 +0200

    [FCT] 1st version of the wheel generation algorithm

---
+++ Assets/Scripts/Generation/WheelGeneration.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generation
+{
+    public class WheelGeneration : MonoBehaviour
+    {
+        private List<GameObject> parts;
+        public WheelPartsSO prefabsParts;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            parts = new List<GameObject>();
+            Generate();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void Generate()
+        {
+            float fraction = 360f / prefabsParts.howMuchPartNeeded;
+            for (int i = 0; i < prefabsParts.howMuchPartNeeded; i++)
+            {
+                parts.Add(GenerateOne(i * fraction));
+            }
+        }
+
+        GameObject GenerateOne(float pitch = 0)
+        {
+            GameObject prefab = Instantiate(prefabsParts.wheelParts[Random.Range(0, prefabsParts.wheelParts.Count)], transform);
+            prefab.transform.Rotate(Vector3.right * pitch);
+            return prefab;
+        }
+    }
+}
---
+++ Assets/Scripts/Generation/WheelPartsSO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generation
+{
+    [CreateAssetMenu(fileName = "WheelPartsSO", menuName = "SO/Generation/WheelParts")]
+    public class WheelPartsSO : ScriptableObject
+    {
+        public List<GameObject> wheelParts;
+        public float howMuchPartNeeded = 60;
+    }
+}

f53448c7f26a39e7e54563412a4811db95e67997
ChristopheClaustre <christophe.claustre.31@gmail.com>
2020-10-03 17:21:43 +0200

    [FCT] 1st version of obstacle generation (1 on each tick, no deletion)

---
+++ Assets/Scripts/Generation/WheelObstacleGeneration.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generation
+{
+    [RequireComponent(typeof(WheelGeneration))]
+    public class WheelObstacleGeneration : MonoBehaviour
+    {
+        public WheelObstaclesSO wheelObstacles;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        
+        public void Tick()
+        {
+            float fraction = 360f / Dora.Inst.behaviourSettings.wheelPartCount;
+            GenerateOne((Behaviour.PlayerMovement.LINE)Random.Range(0, 3), fraction * -(Dora.Inst.behaviourSettings.wheelPartCount / 3)); // Player can see a little more than 1/4 of the wheel -> so appear at 1/3
+        }
+
+        public GameObject GenerateOne(Behaviour.PlayerMovement.LINE line, float pitch)
+        {
+            GameObject prefab = Instantiate(wheelObstacles.wheelObstacles[Random.Range(0, wheelObstacles.wheelObstacles.Count)], transform);
+            prefab.transform.Translate(new Vector3(((int)line - 1) * 3, 0, 0));
+            prefab.transform.localRotation = Quaternion.Inverse(transform.rotation) * Quaternion.Euler(Vector3.right * pitch);
+            
+            return prefab;
+        }
+    }
+}
---
+++ Assets/Scripts/Generation/WheelObstaclesSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generation
+{
+    [CreateAssetMenu(fileName = "WheelObstaclesSO", menuName = "SO/Generation/WheelObstacles")]
+    public class WheelObstaclesSO : ScriptableObject
+    {
+        public List<GameObject> wheelObstacles;
+    }
+}

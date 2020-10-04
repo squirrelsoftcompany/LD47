47af60a54efec9859453a1fccdb286ecb99fd89a
ChristopheClaustre <christophe.claustre.31@gmail.com>
2020-10-04 17:34:57 +0200

    [FCT] prefab and script for Crowd

---
+++ Assets/Scripts/UI/CrowdManagement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdManagement : MonoBehaviour
+{
+    public List<GameObject> Crowd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Tick()
+    {
+        float step = (Dora.Inst.behaviourSettings.fameMax - Dora.Inst.behaviourSettings.fameMin) / (Crowd.Count);
+        for (int i = 0; i < Crowd.Count; i++)
+        {
+            if (Dora.Inst.gameState.currentFame >= Dora.Inst.behaviourSettings.fameMin + i * step)
+            {
+                Crowd[i].SetActive(true);
+            }
+            else
+            {
+                Crowd[i].SetActive(false);
+            }
+        }
+    }
+}

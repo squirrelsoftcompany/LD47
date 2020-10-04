9dc393c378ceb9c3f8de013e39c7ccd23510d8cd
ChristopheClaustre <christophe.claustre.31@gmail.com>
2020-10-04 14:05:51 +0200

    [FCT] Mainscreen !! (alpha version)

---
+++ Assets/Scripts/UI/MainScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class MainScreen : MonoBehaviour
+    {
+        public GameEvent runEvent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (!Dora.Inst.gameState.showMainScreen)
+                runEvent.Raise();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                runEvent.Raise();
+        }
+
+        public void Run()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

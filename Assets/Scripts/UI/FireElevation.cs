613271dcfcd684bf07f331593a870d18dd9e3c3c
ChristopheClaustre <christophe.claustre.31@gmail.com>
2020-10-05 23:09:33 +0200

    [FCT] Fire elevation !!

---
+++ Assets/Scripts/UI/FireElevation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireElevation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.gravity = Vector3.up * Mathf.Clamp(Dora.Inst.gameState.currentWheelSpeed / 4, 0.5f, 4);
+    }
+}

a40b12e37a5f327003501a958203bdf352f28daf
ChristopheClaustre <christophe.claustre.31@gmail.com>
2020-10-17 02:11:06 +0200

    [FCT] Fire sheaf on a successful slide

--- Assets/Scripts/UI/FireElevation.cs
+++ Assets/Scripts/UI/FireElevation.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FireElevation : MonoBehaviour
 {
+    public float sheafImpact = 5;
+    private float sheaf = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class FireElevation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Physics.gravity = Vector3.up * Mathf.Clamp(Dora.Inst.gameState.currentWheelSpeed / 4, 0.5f, 4);
+        var gravity = Vector3.up * Mathf.Clamp(Dora.Inst.gameState.currentWheelSpeed / 4, 0.5f, 4);
+        if (sheaf > 0)
+        {
+            gravity += Vector3.up * sheaf;
+            sheaf -= sheafImpact * Time.deltaTime;
+        }
+        Physics.gravity = gravity;
+    }
+
+    public void InitiateSheaf()
+    {
+        sheaf = sheafImpact;
     }
 }
